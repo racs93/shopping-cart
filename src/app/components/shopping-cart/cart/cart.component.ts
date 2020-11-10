08597457862eec89dc7e835cbf56b154b8f2cc15
Rodrigo <rodrigocorrea2@hotmail.com>
2020-11-09 18:22:37 -0600

    Shoping card more dinamic

--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  cartItems = [
+    { id: 1, productId: 1, productName: 'Set de dados', qty: 5, price: 100 },
+    { id: 2, productId: 2, productName: 'Figuras de accion', qty: 5, price: 50 },
+    { id: 3, productId: 3, productName: 'Manual de mounstros', qty: 3, price: 150 },
+    { id: 4, productId: 4, productName: 'Manual de dungeon master', qty: 2, price: 200 },
+  ];
+
+  cartTotal = 0
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.cartItems.forEach(item => {
+      this.cartTotal += (item.qty * item.price)
+    })
   }
 
 }
