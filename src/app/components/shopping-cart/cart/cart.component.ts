8ad490417eceeb4dfb1efe72396fefaa8385883c
Rodrigo <rodrigocorrea2@hotmail.com>
2020-11-10 10:49:35 -0600

    finishing the shooping cart

--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product'
+import { MessengerService } from 'src/app/services/messenger.service'
 
 @Component({
   selector: 'app-cart',
@@ -8,17 +10,42 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
 
   cartItems = [
-    { id: 1, productId: 1, productName: 'Set de dados', qty: 5, price: 100 },
-    { id: 2, productId: 2, productName: 'Figuras de accion', qty: 5, price: 50 },
-    { id: 3, productId: 3, productName: 'Manual de mounstros', qty: 3, price: 150 },
-    { id: 4, productId: 4, productName: 'Manual de dungeon master', qty: 2, price: 200 },
   ];
 
   cartTotal = 0
 
-  constructor() { }
+  constructor(private msg: MessengerService) { }
 
   ngOnInit() {
+
+    this.msg.getMsg().subscribe((product: Product) => {
+      this.addProductToCart(product)
+    })
+
+  }
+
+  addProductToCart(product: Product) {
+
+    let productExists = false
+
+    for(let i in this.cartItems) {
+      if (this.cartItems[i].productId === product.id) {
+        this.cartItems[i].qty++
+        productExists = true
+        break;
+      }
+    }
+
+    if(!productExists) {
+      this.cartItems.push({
+        productId: product.id,
+        productName: product.name,
+        qty: 1,
+        price: product.price,
+      })
+    }
+
+    this.cartTotal = 0
     this.cartItems.forEach(item => {
       this.cartTotal += (item.qty * item.price)
     })
